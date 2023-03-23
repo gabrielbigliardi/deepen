6768f07a9859aa37ca76eaa6e373d89854f942b8
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2023-03-22 23:55:49 -0300

    icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { AboutComponent } from './components/views/home/about/about.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SolutionsComponent } from './components/views/home/solutions/solutions.component';
 
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule} from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 import { BlogComponent } from './components/views/home/blog/blog.component';
 import { ClientsComponent } from './components/views/home/clients/clients.component';
 import { ContactComponent } from './components/views/home/contact/contact.component';
+import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 
 @NgModule({
@@ -35,6 +37,8 @@ import { ContactComponent } from './components/views/home/contact/contact.compon
     BrowserAnimationsModule,
     MatGridListModule,
     MatCardModule,
+    MatIconModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/template/footer/footer.component.ts
+++ src/app/components/template/footer/footer.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { faInstagram, faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+
+  faLinkedin = faLinkedin
+  faInstagram = faInstagram
+  faFacebook = faFacebook
+  faArrowLeft = faArrowLeft
 
 }
