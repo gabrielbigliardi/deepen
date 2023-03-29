1fe7f0d32557b7b45c4f786dd251d54680f19acf
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2023-03-29 18:41:37 -0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 
-const routes: Routes = [];
+
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([])], 
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/components/template/header/header.component.ts
+++ src/app/components/template/header/header.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   
   isMenuOpen = false;
 
+  
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+
+
+  ngOnInit(): void {
+    this.activatedRoute.fragment.subscribe((value) => {
+      console.log(value)
+      this.jumpTo(value)
+    })
+  }
+
+
+
+  jumpTo(section: any) { //any 
+    document.getElementById(section)?.scrollIntoView({behavior: 'smooth'})
   }
 }
